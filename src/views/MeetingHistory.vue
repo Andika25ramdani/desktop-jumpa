99fec2724d4ac44d247f34ceb2ed43c5d1555c9e
Andika .R <andika15ramdani@gmsil.com>
2021-11-19 11:25:52 +0700

    20211119 - 1125 - page history (belum click)

--- src/views/MeetingHistory.vue
+++ src/views/MeetingHistory.vue
@@ -1,49 +1,22 @@
 <template>
   <div class="meet-history">
 		<h2 class="page-header">Meeting History</h2>
-		<v-simple-table>
-			<template v-slot:default>
-				<thead>
-					<tr>
-						<th>Date/Time</th>
-						<th>Subject</th>
-						<th>Meeting ID</th>
-						<th>Status</th>
-						<th></th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr
-						v-for="item in meetingHistories"
-						:key="item.name"
-					>
-						<td>{{ item.date }} {{ item.time }}</td>
-						<td>{{ item.subject }}</td>
-						<td>{{ item.id }}</td>
-						<td>{{ item.status }}</td>
-						<td></td>
-					</tr>
-				</tbody>
-			</template>
-		</v-simple-table>
-		<!-- <table>
-			<thead>
-				<td>Date/Time</td>
-				<td>Subject</td>
-				<td>Meeting ID</td>
-				<td>Status</td>
-				<td></td>
-			</thead>
-			<tbody>
-				<tr>
-					<td>28/09/2021 11:55</td>
-					<td>Pengukuhan Pengurus Ikal dan Rapat Ke-IV.....</td>
-					<td>2805184</td>
-					<td>Cancelled</td>
-					<td><button>Actions</button></td>
-				</tr>
-			</tbody>
-		</table> -->
+		<div class="table-lists-title">
+			<h6 class="tlt">Date/Time</h6>
+			<h6 class="tlt">Subject</h6>
+			<h6 class="tlt">Meeting ID</h6>
+			<h6 class="tlt">Status</h6>
+			<h6 class="tlt"></h6>
+		</div>
+		<div class="table-lists-wrap">
+			<div class="table-list-item" v-for="history in meetingHistories" :key=history.id>
+				<p class="tli-text">{{ history.date }} {{ history.time }}</p>
+				<p class="tli-text second-item">{{ history.subject }}</p>
+				<p class="tli-text">{{ history.id }}</p>
+				<p class="tli-text" :class="{'cancel': history.status == 'cancelled', 'end': history.status == 'ended'}">{{ history.status }}</p>
+				<button class="last-item white-button">Action</button>
+			</div>
+		</div>
   </div>
 </template>
 
@@ -61,6 +34,62 @@ export default {
 				},
 				{
 					id: 2805185,
+					date: '28/09/2021',
+					time: '11:55',
+					subject: 'Pengukuhan Pengurus Ikal dan Rapat Ke-IV.....',
+					status: 'ended'
+				},
+				{
+					id: 2805186,
+					date: '28/09/2021',
+					time: '11:55',
+					subject: 'Pengukuhan Pengurus Ikal dan Rapat Ke-IV.....',
+					status: 'cancelled'
+				},
+				{
+					id: 2805187,
+					date: '28/09/2021',
+					time: '11:55',
+					subject: 'Pengukuhan Pengurus Ikal dan Rapat Ke-IV.....',
+					status: 'ended'
+				},
+				{
+					id: 2805188,
+					date: '28/09/2021',
+					time: '11:55',
+					subject: 'Pengukuhan Pengurus Ikal dan Rapat Ke-IV.....',
+					status: 'cancelled'
+				},
+				{
+					id: 2805189,
+					date: '28/09/2021',
+					time: '11:55',
+					subject: 'Pengukuhan Pengurus Ikal dan Rapat Ke-IV.....',
+					status: 'ended'
+				},
+				{
+					id: 2805190,
+					date: '28/09/2021',
+					time: '11:55',
+					subject: 'Pengukuhan Pengurus Ikal dan Rapat Ke-IV.....',
+					status: 'cancelled'
+				},
+				{
+					id: 2805191,
+					date: '28/09/2021',
+					time: '11:55',
+					subject: 'Pengukuhan Pengurus Ikal dan Rapat Ke-IV.....',
+					status: 'ended'
+				},
+				{
+					id: 2805192,
+					date: '28/09/2021',
+					time: '11:55',
+					subject: 'Pengukuhan Pengurus Ikal dan Rapat Ke-IV.....',
+					status: 'cancelled'
+				},
+				{
+					id: 2805193,
 					date: '28/09/2021',
 					time: '11:55',
 					subject: 'Pengukuhan Pengurus Ikal dan Rapat Ke-IV.....',
