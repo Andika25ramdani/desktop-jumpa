811940ebb1321e9e9af2f8c5a1a48ffaddbe1862
Andika <andika15ramdani@gmail.com>
2021-11-25 10:54:33 +0700

    Merge branch 'dev' of http://103.157.46.60/dev22/desktop-jumpa into dev

---
+++ src/js/applySign.js
@@ -0,0 +1,14 @@
+import CONFIG from './config'
+import helper from './api_helper'
+
+const { appID, appSecret, accessToken } = CONFIG
+const { sortObj, generate_signature } = helper
+
+export default {
+    applySign: function (obj_param, timeStamp, body_param = null) {
+        let obj_public = {appID, appSecret, timeStamp, accessToken};
+        let obj = Object.assign({}, obj_public, obj_param);
+        let obj_sign = sortObj(obj);
+        return generate_signature(obj_sign, body_param);
+    }
+}
