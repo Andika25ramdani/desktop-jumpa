66e80c61230c9f145ccdd135930dcb0f346bd9a5
Dwi Fitriana <dwirizkyf9o@gmail.com>
2021-11-25 13:08:36 +0700

    login vuex

---
+++ src/store/modules/auth.js
@@ -0,0 +1,42 @@
+import { createStore } from 'vuex'
+import API from '../../js/api_interface'
+const intialState = () => {
+    return {
+        isAuth: false,
+        accessToken: '',
+        account: '',
+        email: '',
+        accountInfo: {}
+    }
+}
+
+export default {
+    namespaced: true,
+    state: intialState(),
+    mutations: {
+        SET: (state, [key, value]) => state[key] = value,
+        RESET: (state) => Object.assign(state, intialState())
+    },
+    getters: {
+        isAuth: (state) => () => state.isAuth,
+        getToken: (state) => () => state.accessToken,
+        getAccount: (state) => () => state.account,
+        getEmail: (state) => () => state.email,
+        getAccountInfo: (state) => () => state.accountInfo,
+    },
+    actions: {
+        signIn: async function({commit}, payload) {
+            const res = await API.account_login(payload.account, payload.password);
+            let data = res.data
+            const { accessToken, account, email } = data.data
+            if (data.retCode == 0) {
+                localStorage.setItem('accessToken', accessToken)
+                commit('SET', ['accessToken', accessToken])
+                commit('SET', ['account', account])
+                commit('SET', ['email', email])
+            }
+
+            return data
+        }
+    }
+}
