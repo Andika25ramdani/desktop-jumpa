d54dea48b5ed528b1a190398a0024aa352e719c9
Fajar Farisya <fajarfarisya@gmail.com>
2021-11-25 11:23:39 +0700

    update particle js

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,80 @@
 <template>
   <red-background/>
+  <!-- <Particles
+      id="tsparticles"
+      :particlesInit="particlesInit"
+      :particlesLoaded="particlesLoaded"
+      :options="{
+        background: {
+          color: {
+            value: '#790F19',
+          },
+        },
+        fpsLimit: 60,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: false,
+              mode: 'push',
+            },
+            onHover: {
+              enable: false,
+              mode: 'repulse',
+            },
+            resize: true,
+          },
+          modes: {
+            bubble: {
+              distance: 400,
+              duration: 2,
+              opacity: 0.8,
+              size: 10,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: '#ffffff',
+          },
+          links: {
+            color: '#ffffff',
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: 'none',
+            enable: true,
+            random: false,
+            speed: 0.5,
+            straight: false,
+            out_mode:'out'
+          },
+          number: {
+            value: 80,
+          },
+          opacity: {
+            value: 0.4,
+          },
+          shape: {
+            type: 'circle',
+          },
+          size: {
+            random: true,
+            value: 2,
+          },
+        },
+        detectRetina: true,
+      }"
+    /> -->
 </template>
 <script>
 import RedBackground from './components/RedBackground.vue'
