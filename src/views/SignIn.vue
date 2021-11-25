b85980847dab7e385f60e0ea3349c5d69e548b2b
Dwi Fitriana <dwirizkyf9o@gmail.com>
2021-11-25 10:46:34 +0700

    migrate api

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -3,8 +3,8 @@
 		<div class="bg-white rounded-px5 p-px25 shadow-custom">
 			<h2 class="font-bold text-grey-dark text-2xl text-center mb-5">Sign In</h2>
 			<form @submit.prevent="onSubmit" class="flex flex-col gap-2.5">
-					<input type="email" name="email" id="email"  placeholder="Email" class="border border-grey-ce rounded-px5 py-2 px-4 min-w-252 outline-none text-xs">
-					<input type="password" name="password" id="password"  placeholder="Password" class="border border-grey-ce rounded-px5 py-2 px-4 min-w-252 outline-none text-xs">
+					<input v-model="signInForm.email" type="email" name="email" id="email"  placeholder="Email" class="border border-grey-ce rounded-px5 py-2 px-4 min-w-252 outline-none text-xs">
+					<input v-model="signInForm.password" type="password" name="password" id="password"  placeholder="Password" class="border border-grey-ce rounded-px5 py-2 px-4 min-w-252 outline-none text-xs">
         <p class="text-xs underline text-grey-dark">
           <router-link to="/">Forgot your password?</router-link>
         </p>
@@ -17,7 +17,7 @@
 	</div>
 </template>
 <script>
-import axios from 'axios'
+import API  from '../js/api_interface'
 export default {
 	name: 'SignIn',
 	data(){
@@ -32,14 +32,13 @@ export default {
 		}
 	},
 	mounted () {
-    axios
-      .get('https://api.ipvideotalk.com/api/v1.0.0/account/login')
-      .then(response => (this.info = response))
-			.catch(error => {
-        console.log(error)
-        this.errored = true
-      })
-      .finally(() => this.loading = false)
+  },
+  methods: {
+	  onSubmit: async function () {
+		console.log(this.signInForm.email, this.signInForm.password)
+		let res = await API.account_login(this.signInForm.email, this.signInForm.password)
+		console.log(res)
+	  }
   }
 }
 </script>
