9558389ba110c148f7b006fded0daac6176a2dc0
Andika <andika15ramdani@gmail.com>
2021-12-17 10:24:15 +0700

    20211217 - reset project

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
-import './assets/sass/jumpa.sass'
-import './assets/tailwind.css'
-
-createApp(App).use(store).use(router).mount('#app')
